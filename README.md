693695cdebc77ef4fcf905f28f54725bd3d9d870
myfreess <neomyfreess@outlook.com>
2025-07-03 16:27:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# moonc_wasm
+
+WebAssembly based MoonBit Compiler
+
+# install
+
+```shell
+cargo add --git https://github.com/moonbitlang/moonc_wasm
+```
+
++ core version: `44c8d2b91f51e0e167ce941ae7522a5f4701bfef`
++ moon version: `5330f33a8435e5b0c2bc775065eaa7e6bfcf1ecb`
---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+mod wasmoo_extern;
+
+fn run_wasmoo(argv: Vec<String>) -> anyhow::Result<()> {
+    let isolate = &mut v8::Isolate::new(Default::default());
+    isolate.set_capture_stack_trace_for_uncaught_exceptions(true, 10);
+    let scope = &mut v8::HandleScope::new(isolate);
+    let context = v8::Context::new(scope, Default::default());
+    // setup file descriptor table
+    context.set_slot(wasmoo_extern::FdTable::new());
+    // setup directory table
+    // context.set_slot(wasmoo_extern::DirTable::new());
+    let scope = &mut v8::ContextScope::new(scope, context);
+
+    let global_proxy = scope.get_current_context().global(scope);
+    wasmoo_extern::init_wasmoo(global_proxy, scope);
+
+    let process_argv = v8::Array::new(scope, argv.len() as i32);
+    for (i, s) in argv.iter().enumerate() {
+        let s = v8::String::new(scope, s).unwrap();
+        process_argv.set_index(scope, i as u32, s.into());
+    }
+    let ident = v8::String::new(scope, "process_argv").unwrap();
+    global_proxy.set(scope, ident.into(), process_argv.into());
+
+    let mut script = String::new();
+    let js_glue = include_str!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/src/moonc/js_glue_for_wasmoo.js"
+    ));
+    script.push_str(js_glue);
+    let code = v8::String::new(scope, &script).unwrap();
+    let name = v8::String::new(scope, "moonc").unwrap();
+    let origin = v8::ScriptOrigin::new(
+        scope,
+        name.into(),
+        0,
+        0,
+        false,
+        0,
+        None,
+        false,
+        false,
+        false,
+        None,
+    );
+    let code = v8::Script::compile(scope, code, Some(&origin)).unwrap();
+    code.run(scope);
+    Ok(())
+}
+
+fn initialize_v8() -> anyhow::Result<()> {
+    v8::V8::set_flags_from_string(&format!("--stack-size={}", 102400));
+    v8::V8::set_flags_from_string("--experimental-wasm-exnref");
+    v8::V8::set_flags_from_string("--experimental-wasm-imported-strings");
+    let platform = v8::new_default_platform(0, false).make_shared();
+    v8::V8::initialize_platform(platform);
+    v8::V8::initialize();
+    Ok(())
+}
+
+pub fn run_moonc(argv: Vec<String>) -> anyhow::Result<()> {
+    initialize_v8()?;
+    run_wasmoo(argv)
+}
